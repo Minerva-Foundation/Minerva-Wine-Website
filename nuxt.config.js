6bd91c218536b84be604fb9e5f9e6e737f9cec26
Jonas Bothin <smitedeluxe@gmail.com>
2022-02-13 21:00:39 +0100

    new dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  generate: {
+    exclude: [/club/]
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
