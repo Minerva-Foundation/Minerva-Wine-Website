b77e659c5859f4af2b043ebc3226170c475f8eec
Jonas Bothin <smitedeluxe@gmail.com>
2022-03-21 11:08:18 +0100

    article links

--- components/portableTextComps/portableLink.vue
+++ components/portableTextComps/portableLink.vue
@@ -1,3 +1,10 @@
 <template>
   <a target="_blank" class="text" :href="$attrs.href"><slot /></a>
 </template>
+
+<style lang="scss" scoped>
+a {
+  color: $secondary;
+  text-decoration: underline;
+}
+</style>
